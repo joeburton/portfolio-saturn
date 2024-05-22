87436f968dd504db643214ee2f9c39192787fb5b
Joe Burton <joeburton@gmail.com>
2024-05-22 11:02:26 +0100

    chore: fixed test

--- src/components/boilerplate.test.tsx
+++ src/components/boilerplate.test.tsx
@@ -6,7 +6,7 @@ describe("Boilerplate", () => {
       getApples: () => 42,
     };
     const spy = vi.spyOn(cart, "getApples").mockReturnValue(10);
-
+    console.log(cart.getApples());
     expect(spy).toHaveBeenCalled();
     expect(cart.getApples()).toEqual(10);
   });
